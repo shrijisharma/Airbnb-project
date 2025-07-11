1b8de97a6b2ecbafdcebdb685eeeae14b830031c
shrijisharma <shrijisharma462@gmail.com>
2025-07-10 12:41:39 -1200

    fixing all

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ const Review = require("./models/review.js");
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.redirectUrl=req.originalUrl;
-        req.flash("error","you must be logged in to create listing");
+        req.flash("error","You must be logged in to create listing");
         return res.redirect("/login");
        
     }
@@ -25,7 +25,7 @@ module.exports.isOwner=async(req,res,next)=>{
     let {id}=req.params;
     let listing=await Listing.findById(id);
     if(!listing.owner.equals(res.locals.currUser._id)){
-        req.flash("error","you are not a owner of this listing");
+        req.flash("error","You are not a owner of this listing");
         return res.redirect(`/listings/${id}`);
     }
     next();
@@ -56,7 +56,7 @@ module.exports.isReviewAuthor=async(req,res,next)=>{
     let {id,reviewId}=req.params;
     let review=await Review.findById(reviewId);
     if(!review.author.equals(res.locals.currUser._id)){
-        req.flash("error","you are not a author of this review");
+        req.flash("error","You are not a author of this review");
         return res.redirect(`/listings/${id}`);
     }
     next();
