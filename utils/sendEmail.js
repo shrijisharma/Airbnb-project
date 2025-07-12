cbe4d32d4bee1b621c2d000c02213c8e00c8e390
shrijisharma <shrijisharma462@gmail.com>
2025-07-11 23:32:36 -1200

    fixing all routes

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,7 @@
-// const nodemailer = require("nodemailer");
-
-// module.exports = async function sendResetEmail(to, resetLink) {
-//   let transporter = nodemailer.createTransport({
-//     service: "gmail", // ya any SMTP
-//     auth: {
-//       user: "YOUR_EMAIL@gmail.com",
-//       pass: "YOUR_EMAIL_PASSWORD" // App password if 2FA enabled
-//     }
-//   });
-
-//   await transporter.sendMail({
-//     from: '"Yaatra Support" <YOUR_EMAIL@gmail.com>',
-//     to,
-//     subject: "Password Reset",
-//     html: `<p>Click the link below to reset your password:</p>
-//            <a href="${resetLink}">${resetLink}</a>`
-//   });
-// };
 const nodemailer = require("nodemailer");
 
-module.exports = async function sendResetEmail(to, resetLink) {
-  const transporter = nodemailer.createTransport({
+async function sendResetEmail(to, resetLink) {
+  let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
@@ -29,10 +10,16 @@ module.exports = async function sendResetEmail(to, resetLink) {
   });
 
   await transporter.sendMail({
-    from: `"Yaatra Support" <${process.env.EMAIL_USER}>`,
+    from: `"Airbnb Clone Support" <${process.env.EMAIL_USER}>`,
     to,
     subject: "Password Reset",
-    html: `<p>Click the link to reset your password:</p>
-           <a href="${resetLink}">${resetLink}</a>`,
+    html: `
+      <p>You requested a password reset.</p>
+      <p>Click the link below to reset your password:</p>
+      <a href="${resetLink}">${resetLink}</a>
+    `,
   });
-};
+}
+
+module.exports = sendResetEmail;
+
